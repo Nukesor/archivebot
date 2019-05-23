5b77e1a19425d186d50501051989d6c605fe2d35
Arne Beer <arne@twobeer.de>
2019-05-24 00:02:19 +0200

    Fix initdb script and don't overwrite db everytime

--- initdb.py
+++ initdb.py
@@ -1,16 +1,12 @@
 #!/bin/env python
 """Drop and create a new database with schema."""
 
-from sqlalchemy_utils.functions import database_exists, create_database, drop_database
+from sqlalchemy_utils.functions import database_exists, create_database
 from archivebot.db import engine, base
-from archivebot.subscriber import Subscriber # noqa
-from archivebot.file import File # noqa
-
+from archivebot.models.file import File # noqa
+from archivebot.models.subscriber import Subscriber # noqa
 
 db_url = engine.url
-if database_exists(db_url):
-    drop_database(db_url)
-create_database(db_url)
-
-base.metadata.drop_all()
-base.metadata.create_all()
+if not database_exists(db_url):
+    create_database(db_url)
+    base.metadata.create_all()
